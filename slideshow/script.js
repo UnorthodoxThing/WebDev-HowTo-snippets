a76b08309ce8e22539000de68b8e25d1784714bd
Caroline Rozali <UnorthodoxThing@Outlook.com>
2019-05-25 23:12:19 +1000

    Update script

--- slideshow/script.js
+++ slideshow/script.js
@@ -1,26 +1,60 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+'use strict';
+let sliderImages = document.querySelectorAll('.slide'),
+  arrowLeft = document.querySelector('#arrow-left'),
+  arrowRight = document.querySelector('#arrow-right'),
+  current = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+// Clear all images
+function reset() {
+  for (let index of sliderImages) {
+    index.style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+
+// let reset = sliderImages.forEach(function(element) {
+//   element.style.display = 'none';
+// })
+
+// function reset() {
+//   for(let i = 0; i< sliderImages.length; i++) {
+//     sliderImages[i].style.display = 'none';
+//   }
+// }
+
+// Start the slide
+function init() {
+  reset();
+  sliderImages[0].style.display = 'block';
+}
+
+// Show Previous
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = 'block';
+  current--; // Change current state as follows
+}
+
+// Show Next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = 'block';
+  current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener('click', function() {
+  if(current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener('click', function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+init();
